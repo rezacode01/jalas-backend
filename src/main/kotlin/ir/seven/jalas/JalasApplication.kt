dec560c5225ad81f3158fe0a6a2365a6b20394eb
rezacode01 <rezacode01@gmail.com>
2019-11-28 16:13:10 +0330

    init project

---
+++ src/main/kotlin/ir/seven/jalas/JalasApplication.kt
@@ -0,0 +1,11 @@
+package ir.seven.jalas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JalasApplication
+
+fun main(args: Array<String>) {
+    runApplication<JalasApplication>(*args)
+}
