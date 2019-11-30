5fb062e9ed659b2f9ee0cd62c66103607d24c19c
rezacode01 <rezacode01@gmail.com>
2019-11-30 19:24:01 +0330

    add CORS configurations

---
+++ src/main/kotlin/ir/seven/jalas/configurations/CORSConfiguration.kt
@@ -0,0 +1,35 @@
+package ir.seven.jalas.configurations
+
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
+import javax.servlet.*
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class CORSConfiguration : Filter {
+
+    override fun init(fc: FilterConfig) {}
+
+    override fun doFilter(req: ServletRequest, resp: ServletResponse,
+                          chain: FilterChain) {
+        val response = resp as HttpServletResponse
+        val request = req as HttpServletRequest
+        response.setHeader("Access-Control-Allow-Origin", "*")
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT")
+        response.setHeader("Access-Control-Max-Age", "3600")
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN")
+        response.setHeader("Access-Control-Allow-Credentials", true.toString())
+        if ("OPTIONS".equals(request.method, ignoreCase = true)) {
+            response.status = HttpServletResponse.SC_OK
+        } else {
+            chain.doFilter(req, resp)
+        }
+
+    }
+
+    override fun destroy() {}
+
+}
