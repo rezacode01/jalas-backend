1479724484ec31f5eede38bb15f1a0fe5fb67679
rezacode01 <rezacode01@gmail.com>
2019-12-14 20:28:39 +0330

    comment a test

--- src/test/kotlin/JalasTestConfiguration.kt
+++ src/test/kotlin/JalasTestConfiguration.kt
@@ -7,10 +7,10 @@ import org.springframework.boot.runApplication
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
-@SpringBootTest(classes = [JalasApplication::class])
-class JalasTestConfiguration {
-
-    fun contextLoads() {
-    }
-}
+//@RunWith(SpringRunner::class)
+//@SpringBootTest(classes = [JalasApplication::class])
+//class JalasTestConfiguration {
+//
+//    fun contextLoads() {
+//    }
+//}
