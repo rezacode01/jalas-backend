acea5c91da5e5dca08b8fb06f0170cafd3bb48ad
rezacode01 <rezacode01@gmail.com>
2019-12-28 21:29:48 +0330

    add admin authorization for admin stats controller

--- src/main/kotlin/ir/seven/jalas/controllers/AdminController.kt
+++ src/main/kotlin/ir/seven/jalas/controllers/AdminController.kt
@@ -2,6 +2,7 @@ package ir.seven.jalas.controllers
 
 import ir.seven.jalas.services.AdminService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -14,6 +15,7 @@ class AdminController {
     private lateinit var adminService: AdminService
 
     @GetMapping("/general")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     fun getSystemGeneralStats(): Map<String, String> {
         return adminService.getSystemGeneralStats()
     }
